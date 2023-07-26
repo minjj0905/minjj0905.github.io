c91f885bffe614c2e90bd8f17e673837e12b607f
minjj0905 <minjj0905@daum.net>
2023-07-26 11:17:30 +0900

    💄 configure font styles

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -10,20 +10,6 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     padding: 0;
     margin: 0;
-    font-family: 'Pretendard',
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'system-ui',
-    'Roboto',
-    'Helvetica Neue',
-    'Segoe UI',
-    'Apple SD Gothic Neo',
-    'Noto Sans KR',
-    'Malgun Gothic',
-    'Apple Color Emoji',
-    'Segoe UI Emoji',
-    'Segoe UI Symbol',
-    'sans-serif' !important;
   }
 
   body {
