7828a807c2102a250b85b755ce64057a55ef19ae
minjj0905 <minjj0905@daum.net>
2023-07-26 15:32:33 +0900

    🐛 fix: deploy config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   compiler: {
     // 추가
     styledComponents: true, // 추가
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -4,7 +4,7 @@ import loopy from 'public/loopy.png';
 const Test = () => {
   return (
     <div>
-      <Image src={loopy} alt="loopy" />
+      <Image src={loopy} alt="loopy" width={100} />
       테스트
     </div>
   );
