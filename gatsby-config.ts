9585f8d2dfafcbf30df590f12af0bd2ee7c58bb2
minjj0905 <minjj0905@daum.net>
2024-01-30 16:49:35 +0900

    test 글 작성

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -34,7 +34,12 @@ const config: GatsbyConfig = {
         icon: 'src/images/icon.png',
       },
     },
-    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
@@ -52,6 +57,14 @@ const config: GatsbyConfig = {
         path: './src/pages/',
       },
       __key: 'pages',
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'blog',
+        path: './contents/blog/',
+      },
+      __key: 'blog',
     },
   ],
 };
