b02928c2d1c599afd6fb9094a33c2817a8a8ad97
minjj0905 <minjj0905@daum.net>
2024-04-17 17:23:00 +0900

    feat: favicon 변경, header 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -10,11 +10,13 @@ const Header = () => {
   return (
     <header className="sticky top-0 w-full h-16 z-50 bg-c-bg-primary border-b-2 flex justify-center">
       <div className="max-w-[1200px] w-full px-5 flex justify-between items-center ">
-        <h1 className="text-xl font-bold text-left font-gmarket">
-          Minjj
-          <span className="text-c-primary">.</span>
-          log
-        </h1>
+        <Link to="/">
+          <h1 className="text-xl font-bold text-left font-gmarket">
+            Minjj
+            <span className="text-c-primary text-4xl">.</span>
+            log
+          </h1>
+        </Link>
         <nav className="font-bold font-gmarket text-lg">
           <ul className="flex gap-x-6 items-center h-full ">
             <li className="leading-4">
