e62df35dc0d13327626874fe779574d4bfa6047f
minjj0905 <minjj0905@daum.net>
2024-04-17 17:26:53 +0900

    style: nav line-height 통일

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -18,17 +18,17 @@ const Header = () => {
           </h1>
         </Link>
         <nav className="font-bold font-gmarket text-lg">
-          <ul className="flex gap-x-6 items-center h-full ">
-            <li className="leading-4">
+          <ul className="flex gap-x-6 items-center h-full leading-4">
+            <li>
               <Link to="/post">post</Link>
             </li>
-            <li className="leading-4">
+            <li>
               <Link to="/life">life</Link>
             </li>
-            <li className="leading-4">
+            <li>
               <Link to="/about">about</Link>
             </li>
-            <li className="leading-4">
+            <li>
               <button onClick={switchTheme}>
                 <MoonIcon className="w-5 h-5" />
               </button>
