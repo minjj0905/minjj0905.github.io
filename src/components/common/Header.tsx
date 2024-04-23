f3e889b3e335599695292009629696d7af4f42f1
minjj0905 <minjj0905@daum.net>
2024-04-23 17:36:03 +0900

    feat: header 아이콘 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,4 @@
-import { MoonIcon } from '@heroicons/react/20/solid';
+import { MagnifyingGlassIcon, MoonIcon, RssIcon } from '@heroicons/react/20/solid';
 import { Link } from 'gatsby';
 
 const Header = () => {
@@ -8,17 +8,17 @@ const Header = () => {
   };
 
   return (
-    <header className="sticky top-0 w-full h-16 z-50 bg-c-bg-primary border-b-2 flex justify-center">
-      <div className="max-w-[1200px] w-full px-5 flex justify-between items-center ">
+    <header className="sticky top-0 w-full h-16 z-50 bg-c-bg-primary border-b-2 flex justify-center items-center">
+      <div className="max-w-[1200px] h-full w-full px-5 flex justify-between items-center">
         <Link to="/">
           <h1 className="text-xl font-bold text-left font-gmarket">
             Minjj
-            <span className="text-c-primary text-4xl">.</span>
+            <span className="text-c-primary text-3xl leading-4">.</span>
             log
           </h1>
         </Link>
-        <nav className="font-bold font-gmarket text-lg">
-          <ul className="flex gap-x-6 items-center h-full leading-4">
+        <nav className="font-bold font-gmarket text-lg leading-4">
+          <ul className="flex gap-8 items-center h-full">
             <li>
               <Link to="/post">post</Link>
             </li>
@@ -29,8 +29,18 @@ const Header = () => {
               <Link to="/about">about</Link>
             </li>
             <li>
+              <button>
+                <RssIcon className="w-5 h-5" />
+              </button>
+            </li>
+            <li>
               <button onClick={switchTheme}>
                 <MoonIcon className="w-5 h-5" />
+              </button>
+            </li>
+            <li>
+              <button>
+                <MagnifyingGlassIcon className="w-5 h-5" />
               </button>
             </li>
           </ul>
