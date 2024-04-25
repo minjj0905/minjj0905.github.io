9f0de26926e6d384d2ebd43fb1ac869443651c16
minjj0905 <minjj0905@daum.net>
2024-04-25 13:56:21 +0900

    uncomment file

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,38 +1,38 @@
-// import Layout from '@/components/common/Layout';
-// import SEO from '@/components/common/SEO';
-// import { Button } from '@/components/ui/button';
-// import { Link, graphql } from 'gatsby';
+import Layout from '@/components/common/Layout';
+import SEO from '@/components/common/SEO';
+import { Button } from '@/components/ui/button';
+import { Link, graphql } from 'gatsby';
 
-// export const query = graphql`
-//   query {
-//     allMdx {
-//       edges {
-//         node {
-//           frontmatter {
-//             title
-//             date
-//             category
-//             summary
-//             update
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
+export const query = graphql`
+  query {
+    allMdx {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+            category
+            summary
+            update
+          }
+        }
+      }
+    }
+  }
+`;
 
-// const Post = ({ data }: { data: any }) => (
-//   <Layout>
-//     <h1>Post</h1>
-//     {data.allMdx.edges.map((node: any) => (
-//       <h1>{node.node.frontmatter.title}</h1>
-//     ))}
-//     <Link to="/">
-//       <Button>To Index</Button>
-//     </Link>
-//   </Layout>
-// );
+const Post = ({ data }: { data: any }) => (
+  <Layout>
+    <h1>Post</h1>
+    {data.allMdx.edges.map((node: any) => (
+      <h1>{node.node.frontmatter.title}</h1>
+    ))}
+    <Link to="/">
+      <Button>To Index</Button>
+    </Link>
+  </Layout>
+);
 
-// export default Post;
+export default Post;
 
-// export const Head = () => <SEO title="Post" />;
+export const Head = () => <SEO title="Post" />;
