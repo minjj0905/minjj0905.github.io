55dbb51f453b9fed50f196545b047d3c1af972e2
minjj0905 <minjj0905@daum.net>
2024-04-29 15:24:11 +0900

    fix: build window instance error

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const useTheme = (): [string, () => void] => {
-  const preferColorScheme = window && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  const localTheme = localStorage.getItem('theme');
-  const initialTheme = localTheme || preferColorScheme;
+  const [theme, setTheme] = useState('');
 
-  const [theme, setTheme] = useState(initialTheme);
+  useEffect(() => {
+    const preferColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    const localTheme = localStorage.getItem('theme');
+    const initialTheme = localTheme || preferColorScheme;
+    setTheme(initialTheme);
+  }, []);
 
   const setMode = (mode: string) => {
     localStorage.setItem('theme', mode);
